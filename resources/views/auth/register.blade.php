b762e947e2bd4b74c663b9043464daae0f2a82b4
Cristiam Gamarra <gamarra_247@hotmail.com>
2024-04-08 21:58:12 +0200

    aggiornamenti colori

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,31 +1,36 @@
 <x-layout.layout>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
     <div class="mt-5">
     <form method="POST" action="/register">
         @csrf
+        <h3>Registrati</h3>
         <div class="mb-3">
             <label class="form-label color-primary">Name</label>
-            <input type="name" name="name" class="form-control" value="{{old('name')}}">
+            <input type="name" placeholder="Name o username" name="name" class="form-control" value="{{old('name')}}">
             @error('name')
                 <span>{{$message}}</span>
             @enderror
         </div>
         <div class="mb-3">
             <label class="form-label color-primary">Email address</label>
-            <input type="email" name="email" class="form-control" value="{{old('email')}}">
+            <input type="email" placeholder="Email" name="email" class="form-control" value="{{old('email')}}">
             @error('email')
                 <span>{{$message}}</span>
             @enderror
         </div>
         <div class="mb-3">
             <label class="form-label color-primary">Password</label>
-            <input type="password" name="password" class="form-control" >
+            <input type="password" placeholder="Password" name="password" class="form-control" >
             @error('password')
                 <span>{{$message}}</span>
             @enderror
         </div>
         <div class="mb-3">
             <label class="form-label color-primary">Confirmation Password</label>
-            <input type="password" name="password_confirmation" class="form-control">
+            <input type="password" placeholder="Confirma la tua password" name="password_confirmation" class="form-control">
         </div>
         <button type="submit" class="btn background-primary color-accent">Registrati</button>
     </form>
--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,4 +1,4 @@
-
+{{-- 
     <footer class="background-primary text-center">
         <div class="color-accent">
             <h4>Presto S.R.L</h4>
@@ -6,5 +6,5 @@
             <p>Viale Firenze Foligno (PG) 06034 </p>
             <p><i class="fa-brands fa-cc-apple-pay"></i> <i class="fa-brands fa-cc-mastercard"></i> <i class="fa-brands fa-cc-visa"></i> <i class="fa-brands fa-paypal"></i></p>
         </div>
-    </footer>
+    </footer> --}}
 
