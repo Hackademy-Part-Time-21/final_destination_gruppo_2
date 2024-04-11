b937c83ce588ec0300c2fee9b76392779d9eca29
Kate <pancani.caterina@gmail.com>
2024-04-11 18:36:38 +0200

    navbar footer

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,65 +1,30 @@
-<footer class="footer bg-dark footerpers border border-3 border-black">
-    <div class="container-fluid">
-        <div class="row">
-                <div class="col-md-4 text-center pe-5 fw-semibold"> 
-                    <h5 class="text-white fw-bold">Contatti</h5>
-                    <p class="text-white fw-bold">Email: info@presto.it</p>
-                    <p class="text-white fw-bold">Telefono: +39 123456789</p>
-                </div>
-            <div class="col-md-4 text-center me-5">
-                <h5 class="text-white me-3 fw-bolder">Seguici su</h5>
-                <ul class="social-icons row socialColor ps-5 ms-4">
-                    <li class="col-3 fs-2 ps-5 fw-bolder"><a href="https://www.instagram.com/presto.it"><i class="fab fa-instagram"></i></a></li>
-                    <li class="col-3 fs-2 fw-bolder"><a href="https://www.facebook.com/presto.it"><i class="fab fa-facebook"></i></a></li>
-                    <li class="col-3 fs-2 fw-bolder"><a href="https://www.twitter.com/presto_it"><i class="fab fa-twitter"></i></a></li>
+<footer class="footer bg-dark footerpers">
+    <div class="container vw-100">
+        <div class="row text-center">
+            <div class="col-md-6 text-center">
+                <h5 class="text-white fw-bolder">Seguici su</h5>
+                <ul class="wrapper  text-center">
+                    <li class="icon facebook">
+                    <span><i class="fab fa-facebook-f"></i></span>
+                    </li>
+                    <li class="icon youtube">
+                    <span><i class="fab fa-youtube"></i></span>
+                    </li>
+                    <li class="icon tiktok">
+                    <span><i class="fab fa-tiktok"></i></span>
+                    </li>
+                    <li class="icon instagram">
+                    <span><i class="fab fa-instagram"></i></span>
+                    </li>
                 </ul>
             </div>
-            <div class="col-md-3 text-center text-white">
-                <h5 class="fw-bold">Questi siamo noi</h5>
-                <p class="fw-bolder">Presto.it è un sito che si prende cura di te offrendoti i prodotti di alta qualità </p>
-            </div>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <div class="col-md-3 text-center text-white footerColor">
-
-                <h5>Get to Know Us</h5>
-                <ul>
-                    <li>About Us</li>
-                    <li>Careers</li>
-                    <li>Press Releases</li>
-                </ul>
-            </div>
-            <div class="col-md-3 text-white footerColor">
-                <h5>Make Money with Us</h5>
-                <ul>
-                    <li>Sell on Presto.it</li>
-                    <li>Sell on Presto.it Business</li>
-                    <li>Sell Your Apps on Presto.it</li>
-                </ul>
-            </div>
-            <div class="col-md-3 text-white footerColor">
-                <h5>Presto.it Payment Products</h5>
-                <ul>
-                    <li>Presto.it Rewards Visa Signature Cards</li>
-                    <li>Presto.it Store Card</li>
-                    <li>Presto.it Business Card</li>
-                </ul>
-            </div>
-            <div class="col-md-3 text-white footerColor">
-                <h5>Let Us Help You</h5>
-                <ul>
-                    <li>Your Account</li>
-                    <li>Your Orders</li>
-                    <li>Shipping Rates & Policies</li>
-                </ul>
+            <div class="col-md-6 text-center fw-semibold"> 
+                <h5 class="text-white fw-bold">Contatti</h5>
+                    <div class="row">
+                        <p class="col text-white fw-bold">email: info@presto.it</p>
+                        <p class="col text-white fw-bold">phone: +39 123456789</p>
+                    </div>
+                    <p class="text-white fw-bold">address: Via delle case rosse, 48/A - 10010 (TO)</p>
             </div>
         </div>
     </div>
--- resources/views/components/layout/navbar.blade.php
+++ resources/views/components/layout/navbar.blade.php
@@ -1,193 +1,70 @@
-<header id="header" style="border-bottom: 2px solid #411530;">
+<div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <li>
+        <a class="nav-link color-secondary" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-user"></i></a>
+        <ul class="dropdown-menu dropdown-menu-end background-accent">
+            @guest
+                <li>
+                    <a class="dropdown-item" href="{{route('login')}}">Accedi</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="{{route('register')}}">Registrati</a>
+                </li>
+            @endguest
+            @auth
+                <li><a class="dropdown-item" href="#" onclick="event.preventDefault();getElementById('form-logout').submit();">Logout</a>
+                    <form action="/logout" method="POST" id="form-logout">
+                        @csrf
+                    </form>
+                 </li>
+            @endauth
+        </ul>
+    </li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle color-secondary" href="#" id="navCategorie" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Contattaci
+        </a>
+        <ul class="dropdown-menu " aria-labelledby="navCategorie">
+            <li class="nav-item"><a class="nav-link color-primary" href="{{route('lavora-con-noi')}}">Lavora con noi</a></li>
+            <li class="nav-item"><a class="nav-link color-primary" href="{{route('beChecker')}}">Diventa revisore</a></li>
+        </ul>
+    </li>
+    <a href="{{route('ads.index')}}">Annunci</a>
+    <a class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Categorie
+        </a>
+        <ul class="dropdown-menu">
+            @foreach (App\Models\Categories::all() as $category)
+                <li class="nav-item"><a class="nav-link color-primary" href="{{route('adsByCategory',$category->id)}}">{{$category->name}}</a></li>
+            @endforeach
+        </ul>
+    </a>
+  </div>
+  
+  <div id="main" style=" height: 45px; background-color: #111; color: #818181; display:flex; justify-content: space-between;">
+    <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+    <div>
+      <h1 style="font-size:25px"><a href="{{route('home')}}">PRESTO</a> </h1>
+    </div>
+    <div>
+        <form action="{{route('ads.index')}}" method="GET" class="row">
+            <input name ='searched' class="form-control me-2 col" type="search" placeholder="Search" aria-label="Search">
+            <button class="col btn btn-outline-success" type="submit">Search</button>
+        </form>
+</div>
 
-    
-        <div class="col-12 background-primary color-accent" style="height: 30px;">
-            <div class="d-flex justify-content-center align-items-center h-100 px-5">
-                <p class="m-0 w-100 text-center d-md-none">Spedizione gratuita assicurata</p>
-                <p class="m-0 d-none d-md-block text-md-start">Spedizione gratuita assicurata</p>
-        
-                
-                <ul class="list-unstyled d-flex gap-3 m-0 ms-md-auto d-none d-md-flex">
-                    <li>
-                        <a class="link-underline-opacity-25" style="text-decoration: none; color: inherit;" href="">whatsapp</a>
-                    </li>
-                    <li>
-                        <a style="text-decoration: none; color: inherit;" href="">email</a>
-                    </li>
-                    <li>
-                        <a style="text-decoration: none; color: inherit;" href="">tel 3442212133</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        
-        
-        
-        
-        
-        
-        {{-- <div class="col-12 d-flex justify-content-between background-accent color-primary m-" style="height: 68px;"> --}}
-            
-            {{-- <div class="d-flex align-items-center mobile-menu-container" style="position: relative;">  --}}
-                {{-- <div class="d-block d-md-none me-3">
-                </div> --}}
-                    {{-- <button class="btn color-primary ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#menuMobile" aria-expanded="false" aria-controls="menuMobile">
-                        <i class="fa-solid fa-bars fa-lg" style="color: #290f1e;"></i>
-                    </button> --}}
-                    {{-- <div class="collapse" id="menuMobile">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link color-primary"  href="{{route('ads.index')}}" role="button" aria-expanded="false" aria-controls="submenuCategorie">Annunci </a>
-                                
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link color-primary" data-bs-toggle="collapse" href="#submenuCategorie" role="button" aria-expanded="false" aria-controls="submenuCategorie">Categorie <i class="fa-solid fa-caret-down fa-2xs" style="color: #401430;"></i></a>
-                                <div class="collapse" id="submenuCategorie">
-                                    <ul class="nav flex-column">
-                                        @foreach (App\Models\Categories::all() as $category)
-                                            
-                                        <li class="nav-item"><a class="nav-link color-primary" href="#">{{$category->name}}</a></li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link color-primary" data-bs-toggle="collapse" href="#submenuChiSiamo" role="button" aria-expanded="false" aria-controls="submenuChiSiamo">Chi siamo <i class="fa-solid fa-caret-down fa-2xs" style="color: #401430;"></i></a>
-                                <div class="collapse" id="submenuChiSiamo">
-                                    <ul class="nav flex-column">
-                                        <li class="nav-item"><a class="nav-link color-primary" href="#">Informazioni</a></li>
-                                        <li class="nav-item"><a class="nav-link color-primary" href="#">Storia</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link color-primary" data-bs-toggle="collapse" href="#submenuVendi" role="button" aria-expanded="false" aria-controls="submenuVendi">Vendi <i class="fa-solid fa-caret-down fa-2xs" style="color: #401430;"></i></a>
-                                <div class="collapse" id="submenuVendi">
-                                    <ul class="nav flex-column">
-                                        <li class="nav-item"><a class="nav-link color-primary" href="#">Come vendere</a></li>
-                                        <li class="nav-item"><a class="nav-link color-primary" href="#">Regole</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link color-primary" data-bs-toggle="collapse" href="#submenuContattaci" role="button" aria-expanded="false" aria-controls="submenuContattaci">Contattaci <i class="fa-solid fa-caret-down fa-2xs" style="color: #401430;"></i></a>
-                                <div class="collapse" id="submenuContattaci">
-                                    <ul class="nav flex-column">
-                                        <li class="nav-item"><a class="nav-link color-primary" href="#">Form di contatto</a></li>
-                                        <li class="nav-item"><a class="nav-link color-primary" href="#">Altre informazioni</a></li>
-                                        <li><a class="dropdown-item" href="{{route('lavora-con-noi')}}">Lavora con noi</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                        </ul>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownUserMenu">
-                            @guest
-                                <li><a class="dropdown-item" href="{{ route('login') }}">Accedi</a></li>
-                                <li><a class="dropdown-item" href="{{ route('register') }}">Registrati</a></li>
-                            @endguest
-                            @auth
-                                <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Esci</a></li>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            @endauth
-                        </ul>
-                    </div> --}}
-                
-                {{-- <i class="fa-solid fa-lg fa-magnifying-glass ms-3" style="color: #411530; margin: 1rem;"></i> --}}
-            {{-- </div> --}}
-                   
-            
-            {{-- <div style="height: 60px">
-                <a href="{{route('home')}}">
-                    <img src="{{ asset('images/logo.png') }}" alt="Logo" style="height: 55px;">
-                </a>
-            </div> --}}
-            
-            
-            {{-- <div class="d-flex align-items-center me-4">
-                
-                <div class="dropdown">
-                    <a class="fa-solid fa-user fa-lg" style="color: #411530; margin: 1rem; cursor: pointer;" id="dropdownUserMenu" data-bs-toggle="dropdown" aria-expanded="false">
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownUserMenu">
-                        @guest
-                            <li><a class="dropdown-item" href="{{ route('login') }}">Accedi</a></li>
-                            <li><a class="dropdown-item" href="{{ route('register') }}">Registrati</a></li>
-                        @endguest
-                        @auth
-                            <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Esci</a></li>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        @endauth
-                    </ul>
-                </div>
-            
-                <i class="fa-solid fa-cart-shopping fa-lg" style="color: #411530; margin: 1rem;"></i>
-            </div> --}}
-            
-        {{-- </div> --}}
-        
-        
-        <div id="navdesktop" class="py-3 d-none d-md-block" style="height: 58px;">
-            <div class="container">
-                <ul class="nav justify-content-center">
+<script>
+    function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
 
-                    <li class="nav-item dropdown">
-                        <a class="nav-link color-primary hover" href="{{route('ads.index')}}" id="navCategorie" role="button">Annunci</a> 
-                    </li>
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
 
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle color-primary" href="#" id="navCategorie" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Categorie
-                        </a>
-                        <ul class="dropdown-menu " aria-labelledby="navCategorie">
-                            @foreach (App\Models\Categories::all() as $category)
-                                            
-                            <li class="nav-item"><a class="nav-link color-primary" href="{{route('adsByCategory',$category->id)}}">{{$category->name}}</a></li>
-                            @endforeach
-                        </ul>
-                    </li>
-        
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle color-primary" href="#" id="navChiSiamo" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Chi siamo
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navChiSiamo">
-                            <li><a class="dropdown-item" href="#">Informazioni</a></li>
-                            <li><a class="dropdown-item" href="#">Storia</a></li>
-                        </ul>
-                    </li>
-        
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle color-primary" href="#" id="navVendi" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Vendi
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navVendi">
-                            <li><a class="dropdown-item" href="#">Crea annuncio</a></li>
-                            <li><a class="dropdown-item" href="#">FAQ</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle color-primary" href="#" id="navContattaci" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Contattaci
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navContattaci">
-                            <li><a class="dropdown-item" href="#">Form di contatto</a></li>
-                            <li><a class="dropdown-item" href="#">Altre informazioni</a></li>
-                            <li><a class="dropdown-item" href="{{route('lavora-con-noi')}}">Lavora con noi</a></li>
-                        </ul>
-                    </li>
-                    
-                </ul>
-            </div>
-        </div>
-    
-    
-
-</header>
-
-
-
+</script>
