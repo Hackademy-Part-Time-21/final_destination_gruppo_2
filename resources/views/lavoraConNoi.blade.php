3889c8165959da38e1dda7d0647bafd0ebc56947
Federico Evangelista <federicoevangelista92@gmail.com>
2024-04-15 17:54:16 +0200

    frontend vista lavora con noi

--- resources/views/lavoraConNoi.blade.php
+++ resources/views/lavoraConNoi.blade.php
@@ -1,16 +1,15 @@
 <x-layout.layout>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 text-bg-dark">
+
+            <div class="form-login-card">
                 <br>
                 <br>
-                <div class="card text-center pt-3">
+                <div class="card text-center pt-3 color-accent background-secondary form-login-card">
                     <h1>
-                        Non lavorare con noi!
+                        Vuoi lavorare con noi?
                     </h1>
                     <br>
                     <p>
-                        Comunque la mail è finta quindi non arriverà mai neanche la richiesta
+                        Riempi il form, ti risponderemo il prima possibile
                     </p>
                     <br>
                     <br>
@@ -22,25 +21,28 @@
 <br>
                 <form action="{{route('candidati')}}" method="POST" enctype="multipart/form-data">
                     @csrf
-                    <div class="mb-3">
-                        <label class="form-label">Nome e Cognome</label>
+                    <div class="m-3">
+                        <label class="form-label" > <strong>Nome e Cognome</strong></label>
                         <input name="name" type="text" class="form-control">
                     </div>
-                    <div class="mb-3">
-                      <label class="form-label">Indirizzo email</label>
+                    <div class="m-3">
+                      <label class="form-label"><strong>Indirizzo email</strong></label>
                       <input name="email" type="email" class="form-control">
                     </div>
-                    <div class="mb-3">
-                        <label class="form-label">Allega un curriculum (facoltativo)</label>
+                    <div class="m-3">
+                        <label class="form-label"><strong>Allega un curriculum (facoltativo)</strong></label>
                         <input name="cv" class="form-control" type="file" id="formFile">
                     </div>
-                    <div class="mb-3">
-                        <label class="form-label">Presentati brevemente</label>
+                    <div class="m-3">
+                        <label class="form-label"><strong>Presentati brevemente</strong></label>
                         <input name="presentazione" class="form-control" type="text">
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <div class="m-3">
+                        <label class="form-label"><strong>Che lavoro vorresti fare?</strong></label>
+                        <input name="presentazione" class="form-control" type="text">
+                    </div>
+                    <button type="submit" class="btn btn-primary m-3">Submit</button>
                   </form>
             </div>
-        </div>
-    </div>
+      
 </x-layout.layout>
