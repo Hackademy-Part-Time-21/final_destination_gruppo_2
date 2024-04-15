ebf56cb7a4556686725df99c1eb8c3b14f296f55
Federico Evangelista <federicoevangelista92@gmail.com>
2024-04-15 17:43:31 +0200

    front basico di login e register con mediaquery responsive

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,32 +1,36 @@
 <x-layout.layout>
-    <div class="background">
-        <div class="shape"></div>
-        <div class="shape"></div>
-    </div>
-    <form method="POST" action="/login">
-        @csrf
-        <h3>Login Here</h3>
-        <div class="mb-3">
-            <label class="form-label color-primary">Email</label>
-            <input type="email" name="email" placeholder="Email o Phone" class="form-control" value="{{old('email')}}">
-            @error('email')
-                <span>{{$message}}</span>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label class="form-label color-primary">Password</label>
-            <input type="password" placeholder="Password" name="password" class="form-control" >
-            @error('password')
-                <span>{{$message}}</span>
-            @enderror
-        </div>
-        <a href="/register" class="text-decoration-none color-primary">Non sei registrato?</a>
-        <br>
-        <button type="submit" class="btn background-primary color-accent mt-2">Accedi</button>
+    <h3 style="text-align: center">Accedi</h3>
+    <div class="form-login-card background-detail m-5">
+        <form method="POST" action="/login" >
+            @csrf
+            
+            <div class="m-3">
+                <label class="form-label color-primary"> <strong>Email</strong></label>
+                <input type="email" name="email" placeholder="Email o Phone" class="form-control" value="{{old('email')}}">
+                @error('email')
+                    <span>{{$message}}</span>
+                @enderror
+            </div>
+            <div class="m-3">
+                <label class="form-label color-primary"><strong>Password</strong></label>
+                <input type="password" placeholder="Password" name="password" class="form-control" >
+                @error('password')
+                    <span>{{$message}}</span>
+                @enderror
+            </div>
 
-        <div class="social">
-            <div class="go"><i class="fab fa-google"></i>Google</div>
-            <div class="fb"><i class="fab fa-facebook"></i>Facebook</div>
-        </div>
-    </form>
+            <button type="submit" class=" m-3 btn background-primary color-accent mt-2">Accedi</button> 
+            
+            
+            
+            
+            <div class="social mt-4">
+                <div class="go"><i class="fab fa-google ms-3"></i> Google</div>
+                <div class="fb"><i class="fab fa-facebook ms-3 mb-3"></i> Facebook</div>
+            </div>
+
+            <br>
+            <a href="/register" class="m-3 text-decoration-none color-primary">Non sei ancora registrato?</a>
+        </form>
+    </div>
 </x-layout.layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,33 +1,34 @@
 <x-layout.layout>
-    <div class="mt-5">
+    <h3 style="text-align: center">Registrati</h3>
+    <div class="form-login-card background-detail m-5">
     <form method="POST" action="/register">
         @csrf
-        <div class="mb-3">
-            <label class="form-label color-primary">Name</label>
+        <div class="m-3">
+            <label class="form-label color-primary"> <strong>Nome</strong></label>
             <input type="name" name="name" class="form-control" value="{{old('name')}}">
             @error('name')
                 <span>{{$message}}</span>
             @enderror
         </div>
-        <div class="mb-3">
-            <label class="form-label color-primary">Email address</label>
+        <div class="m-3">
+            <label class="form-label color-primary"><strong>Indirizzo Email</strong></label>
             <input type="email" name="email" class="form-control" value="{{old('email')}}">
             @error('email')
                 <span>{{$message}}</span>
             @enderror
         </div>
-        <div class="mb-3">
-            <label class="form-label color-primary">Password</label>
+        <div class="m-3">
+            <label class="form-label color-primary"> <strong>Password</strong></label>
             <input type="password" name="password" class="form-control" >
             @error('password')
                 <span>{{$message}}</span>
             @enderror
         </div>
-        <div class="mb-3">
-            <label class="form-label color-primary">Confirmation Password</label>
+        <div class="m-3">
+            <label class="form-label color-primary"><strong>Conferma Password</strong></label>
             <input type="password" name="password_confirmation" class="form-control">
         </div>
-        <button type="submit" class="btn background-primary color-accent">Registrati</button>
+        <button type="submit" class="btn background-primary color-accent m-3">Registrati</button>
     </form>
 </div>
 </x-layout.layout>
